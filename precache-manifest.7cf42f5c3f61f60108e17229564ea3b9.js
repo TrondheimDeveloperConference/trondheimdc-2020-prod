34f97fb42922f43e1f79970f8f7b854fa920cce0
Marvin Bredal Lillehaug <marvin.lillehaug@gmail.com>
2020-01-08 21:13:19 +0100

    initial commit

---
+++ precache-manifest.7cf42f5c3f61f60108e17229564ea3b9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b841128a7a65a5149db80f5f1cb0e943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6346f26491d5dc5c7c99",
+    "url": "/static/css/main.ba976b29.chunk.css"
+  },
+  {
+    "revision": "698dc3dcdd1e1ae3f586",
+    "url": "/static/js/2.9f778d2c.chunk.js"
+  },
+  {
+    "revision": "bed6aca131def4e9792f6588738790db",
+    "url": "/static/js/2.9f778d2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6346f26491d5dc5c7c99",
+    "url": "/static/js/main.eb74aac2.chunk.js"
+  },
+  {
+    "revision": "3df7d18025a6bc46af19",
+    "url": "/static/js/runtime-main.16ebb460.js"
+  },
+  {
+    "revision": "6b9e6d6bdc548e99eef5c7ce988c9ac8",
+    "url": "/static/media/bg.6b9e6d6b.png"
+  },
+  {
+    "revision": "7752a01532538a5127bf9f9a4bd2a3f9",
+    "url": "/static/media/bg2.7752a015.png"
+  },
+  {
+    "revision": "6b7d051922e55e5cd0cc2784d5a56574",
+    "url": "/static/media/bg_jumbotron.6b7d0519.jpg"
+  },
+  {
+    "revision": "17efa687d8746dbe51c6046199c62ff3",
+    "url": "/static/media/calendar.17efa687.svg"
+  },
+  {
+    "revision": "7941ef8539236f3c9a246d836ebeadc9",
+    "url": "/static/media/coc_background.7941ef85.png"
+  },
+  {
+    "revision": "9f90e12911d7e841d3e00d458f6b94c7",
+    "url": "/static/media/code_jumbotron.9f90e129.png"
+  },
+  {
+    "revision": "cf145a2ceff57c5f678820be6d68cca9",
+    "url": "/static/media/dataforeningen.cf145a2c.svg"
+  },
+  {
+    "revision": "46bb59058ba39a53b02a082fd30eb897",
+    "url": "/static/media/email.46bb5905.svg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/static/media/facebook.34e73505.svg"
+  },
+  {
+    "revision": "597b1cb39bdf308f75efcdb6fc2ea102",
+    "url": "/static/media/hamburger.597b1cb3.svg"
+  },
+  {
+    "revision": "62bf223d2bb23b29a9afbb41d81fee8e",
+    "url": "/static/media/hamburger_closed.62bf223d.svg"
+  },
+  {
+    "revision": "01b7ea06a552892f2fe49d9a5738e574",
+    "url": "/static/media/index_01.01b7ea06.jpg"
+  },
+  {
+    "revision": "b303a4af7375a9a7455fa0cf5c0d090f",
+    "url": "/static/media/index_03.b303a4af.jpg"
+  },
+  {
+    "revision": "89b343c52c0f6cb17e9ede78ea0b193e",
+    "url": "/static/media/ixda.89b343c5.svg"
+  },
+  {
+    "revision": "5703732e5c9962556a6ba7f7a1bc3400",
+    "url": "/static/media/javabin.5703732e.svg"
+  },
+  {
+    "revision": "8fd6210c73a62091c882ed38fcc28a77",
+    "url": "/static/media/nnug.8fd6210c.svg"
+  },
+  {
+    "revision": "c60e0d1c66af442fdd581e2932789209",
+    "url": "/static/media/partners_jumbotron.c60e0d1c.png"
+  },
+  {
+    "revision": "ea7df5ab3fc7e54e2c69b3607cca2a96",
+    "url": "/static/media/programme_jumbotron.ea7df5ab.png"
+  },
+  {
+    "revision": "e9ea64cd264b5a5331da64250f025951",
+    "url": "/static/media/spillmakerlauget.e9ea64cd.svg"
+  },
+  {
+    "revision": "488a9d1ec7f159949dba4fb744275cdf",
+    "url": "/static/media/startups_jumbotron.488a9d1e.png"
+  },
+  {
+    "revision": "35cc8c1b3f87fe3ac935cc3fbdf6ce51",
+    "url": "/static/media/tdc_grayscale.35cc8c1b.png"
+  },
+  {
+    "revision": "4399c36b3f777c560f178fba62a0a183",
+    "url": "/static/media/tdc_jumbotron.4399c36b.png"
+  },
+  {
+    "revision": "e5680f2efafce5f99e145f26a6534cb9",
+    "url": "/static/media/ticket_jumbotron.e5680f2e.png"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/static/media/twitter.a1223de5.svg"
+  }
+]);
